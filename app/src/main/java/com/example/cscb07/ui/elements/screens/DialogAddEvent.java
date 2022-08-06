9dfe9c1a13724d64984a1a57c690a0e181b87209
Gigi <gigi_cheang@hotmail.com>
2022-08-06 15:47:31 -0400

    Created files for dialog functionality Added strings to strings.xml Added additional menu options to bottom navigation

---
+++ app/src/main/java/com/example/cscb07/ui/elements/screens/DialogAddEvent.java
@@ -0,0 +1,30 @@
+package com.example.cscb07.ui.elements.screens;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.cscb07.R;
+
+public class DialogAddEvent extends DialogFragment {
+    // WIP!
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Get the layout inflater
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+//        builder.setView(inflater.inflate(R.layout.dialog_add_event, null));
+        return builder.create();
+    }
+}
