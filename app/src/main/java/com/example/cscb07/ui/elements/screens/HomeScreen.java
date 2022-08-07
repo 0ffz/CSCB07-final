0b39c74d7c47790b762b6fced38ef1fe69420507
Tyler3412 <62577262+Tyler3412@users.noreply.github.com>
2022-08-07 19:44:05 -0400

    Added working recycler view to home screen, item click is still WIP

--- app/src/main/java/com/example/cscb07/ui/elements/screens/HomeScreen.java
+++ app/src/main/java/com/example/cscb07/ui/elements/screens/HomeScreen.java
@@ -9,13 +9,19 @@ import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.cscb07.R;
 import com.example.cscb07.data.models.VenueModel;
 import com.example.cscb07.data.util.FirebaseUtil;
 import com.example.cscb07.data.util.MessageUtil;
+import com.example.cscb07.ui.state.VenueUiState;
 import com.example.cscb07.ui.stateholders.LoginViewModel;
 import com.google.common.collect.ImmutableList;
 import com.google.firebase.auth.FirebaseAuth;
@@ -27,9 +33,10 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class HomeScreen extends Fragment {
+public class HomeScreen extends Fragment implements RecyclerAdapt.ItemClickListener {
     private NavController navController;
     private LoginViewModel loginViewModel;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -50,7 +57,28 @@ public class HomeScreen extends Fragment {
                     MessageUtil.showError(R.string.error_email);
                 });
                 // TODO make home screen
+                ArrayList<VenueUiState> state = new ArrayList<VenueUiState>();
+                for(int i = 0; i < 10; i++)
+                {
+                    state.add(new VenueUiState("Venue ", "Description"));
+                }
+                RecyclerView r = view.findViewById(R.id.venues_container);
+                RecyclerAdapt recyclerAdapt = new RecyclerAdapt(state, this);
+                r.setAdapter(recyclerAdapt);
+                r.setLayoutManager(new LinearLayoutManager(this.getActivity()));
             }
         });
+    }
+
+    @Override
+    public void onItemClick(VenueUiState dataModel) {
+        //Fragment fragment = EventsFragment.newInstance(dataModel.getName());
+        //FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+        //transaction.replace(R.id.venues_container, fragment, "events_fragment");
+
+        //transaction.hide(getActivity().getSupportFragmentManager().findFragmentByTag("main_fragment"));
+        //transaction.add(R.id.frame_container, fragment);
+        //transaction.addToBackStack(null);
+        //transaction.commit();
     }
 }
--- app/src/main/java/com/example/cscb07/ui/state/VenueUiState.java
+++ app/src/main/java/com/example/cscb07/ui/state/VenueUiState.java
@@ -1,8 +1,27 @@
 package com.example.cscb07.ui.state;
 
+import java.util.List;
+
 public class VenueUiState {
     //TODO
 //    List<String> sports;
 //    List<Event> upcomingEvents;
+    final String description;
+    final String name;
+
+    public VenueUiState(String name, String description)
+    {
+        this.name = name;
+        this.description = description;
+    }
+
+    public final String getName()
+    {
+        return this.name;
+    }
+    public final String getDescription()
+    {
+        return this.description;
+    }
 }
 
