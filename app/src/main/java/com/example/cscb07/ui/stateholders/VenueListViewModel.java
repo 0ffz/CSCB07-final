c2436c194878dd7f08cd74d1896edea8d0d5595b
Gigi <gigi_cheang@hotmail.com>
2022-08-08 15:56:06 -0400

    Implemented venue list

--- app/src/main/java/com/example/cscb07/ui/stateholders/VenueListViewModel.java
+++ app/src/main/java/com/example/cscb07/ui/stateholders/VenueListViewModel.java
@@ -22,48 +22,26 @@ import java.util.stream.Collectors;
 
 import com.example.cscb07.ui.state.EventUiState;
 import com.example.cscb07.ui.state.VenueUiState;
+
 import io.vavr.Value;
 import io.vavr.control.Try;
 
 public class VenueListViewModel {
-    private final VenueRepository eventRepository = ServiceLocator.getInstance().getEventRepository();
-    private final MutableLiveData<VenueUiState> venue = new MutableLiveData<>();
-    private final MediatorLiveData<List<VenueUiState>> events = new MediatorLiveData<>();
+    private final VenueRepository venueRepository = ServiceLocator.getInstance().getVenueRepository();
+    private final MutableLiveData<List<VenueUiState>> venues = new MutableLiveData<>();
 
-    public VenueListViewModel() {
-        // Clear loaded events when venue changes
-        events.addSource(venue, newVenue -> events.setValue(
-                // TODO update function call
-                eventRepository.getVenues(null, null, null, result -> {
-                    result.onSuccess(newEvents -> {
-                        events.setValue(
-                            newEvents.stream()
-                                    .map(it -> new VenueUiState(it.model.name, it.model.description, it.id))
-                                    .collect(Collectors.toList());
-                        );
-                    }).onFailure(f -> MessageUtil.showError(R.string.error_fail_to_get_events));
-                });
-        ));
+    public LiveData<List<VenueUiState>> getVenues() {
+        return venues;
     }
 
-    public LiveData<List<EventModel>> getEvents() {
-        return events;
-    }
-
-    public void loadMoreEvents() {
-
-         eventRepository.getEventsForVenue(lastEvent.getValue(), venueId, 10, lists -> {
-             List<WithId<EventId, EventModel>> e = lists.get(); //get list from getEventsForVenue
-             List<EventModel> eventList = new ArrayList<>();
-             for(WithId<EventId, EventModel> event: e){
-                 eventList.add(event.model);
-             }
-             if(e.size() == 0)
-                 lastEvent.setValue(new EventId("")); //reset lastEvent if the list is empty
-             else {
-                 lastEvent.setValue(e.get(e.size() - 1).id); //sets lastEvent to last id in the list
-                 events.setValue(eventList);
-             }
+    public void loadVenues() {
+        venueRepository.getVenues(null, 10, "", result -> {
+            result.onSuccess(newVenues -> {
+                venues.setValue(newVenues.stream()
+                        .map(it -> new VenueUiState(it.model.name, it.model.description, it.id))
+                        .collect(Collectors.toList()));
+            });
+            result.onFailure(f -> MessageUtil.showError(R.string.error_fail_to_get_events));
         });
     }
 }
