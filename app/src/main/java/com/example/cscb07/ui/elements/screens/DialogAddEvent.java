af73630869bb81c8ca166e9a86a3128a3162d4be
0ffz <dan.voznyy@gmail.com>
2022-08-08 18:33:47 -0400

    Setup fragment code for add event dialog

--- app/src/main/java/com/example/cscb07/ui/elements/screens/DialogAddEvent.java
+++ app/src/main/java/com/example/cscb07/ui/elements/screens/DialogAddEvent.java
@@ -2,24 +2,20 @@ package com.example.cscb07.ui.elements.screens;
 
 import android.os.Bundle;
 import android.text.InputType;
-import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
-import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import com.example.cscb07.R;
 import com.google.android.material.datepicker.MaterialDatePicker;
-import com.google.android.material.textfield.TextInputLayout;
 import com.google.android.material.timepicker.MaterialTimePicker;
 import org.jetbrains.annotations.NotNull;
 
-import java.sql.Date;
-import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.function.Consumer;
 
 public class DialogAddEvent extends Fragment {
     @Override
@@ -27,45 +23,52 @@ public class DialogAddEvent extends Fragment {
         return inflater.inflate(R.layout.dialog_add_event, container, false);
     }
 
-    public void showDatePicker(TextView date) {
-            MaterialDatePicker<Long> picker = MaterialDatePicker.Builder.datePicker()
-                    .setTitleText("Select date")
-                    .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
-                    .build();
-            picker.addOnPositiveButtonClickListener(result -> {
-                date.setText(SimpleDateFormat.getDateInstance().format(new Date(result)));
-            });
-            picker.show(requireActivity().getSupportFragmentManager(), "date");
+    public void showDatePicker(Consumer<Date> consumer) {
+        MaterialDatePicker<Long> picker = MaterialDatePicker.Builder.datePicker()
+                .setTitleText(R.string.dialog_select_date)
+                .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                .build();
+        picker.addOnPositiveButtonClickListener(result -> consumer.accept(new Date(result)));
+        picker.show(requireActivity().getSupportFragmentManager(), "date");
     }
 
-    public void showTimePicker(TextView time) {
+    public void showTimePicker(Consumer<Long> consumer) {
         MaterialTimePicker picker = new MaterialTimePicker.Builder()
-                .setTitleText("Select time")
+                .setTitleText(R.string.dialog_select_time)
                 .setInputMode(MaterialTimePicker.INPUT_MODE_CLOCK)
                 .build();
-        picker.addOnPositiveButtonClickListener(result -> {
-            time.setText(picker.getHour() + ":" + picker.getMinute());
-        });
+        // TODO pass TimeUiState object
+        picker.addOnPositiveButtonClickListener(result -> consumer.accept(-1L));
         picker.show(requireActivity().getSupportFragmentManager(), "clock");
+    }
+
+    public void setupField(EditText field, Runnable onSelected) {
+        field.setInputType(InputType.TYPE_NULL);
+        field.setOnFocusChangeListener((v, hasFocus) -> {
+            if (hasFocus) onSelected.run();
+        });
+        field.setOnClickListener((v) -> onSelected.run());
     }
 
     @Override
     public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         TitleBarUtil.setupTitleBar(this);
-        TextInputLayout layout = view.findViewById(R.id.event_start_date_layout);
-//        layout.oninp
-        EditText startDate = view.findViewById(R.id.event_start_date);
-        startDate.setInputType(InputType.TYPE_NULL);
-        startDate.setOnFocusChangeListener((v, hasFocus) -> {
-            if (hasFocus) showDatePicker(startDate);
-        });
-        startDate.setOnClickListener((v) -> showDatePicker(startDate));
 
+        EditText startDate = view.findViewById(R.id.event_start_date);
         EditText startTime = view.findViewById(R.id.event_start_time);
-        startTime.setInputType(InputType.TYPE_NULL);
-        startTime.setOnClickListener(v -> showTimePicker(startTime));
-        startTime.setOnFocusChangeListener((v, hasFocus) -> {
-            if (hasFocus) showTimePicker(startTime);
-        });
+        EditText endDate = view.findViewById(R.id.event_end_date);
+        EditText endTime = view.findViewById(R.id.event_end_time);
+
+        setupField(startDate, () -> showDatePicker(date -> { /* TODO set date in viewmodel */ }));
+        setupField(startTime, () -> showTimePicker(time -> {
+        }));
+        setupField(endDate, () -> showDatePicker(date -> {
+        }));
+        setupField(endTime, () -> showTimePicker(time -> {
+        }));
+
+        //TODO observe LiveData from viewmodel
+//        date.setText(SimpleDateFormat.getDateInstance().format(new Date(result)));
+//        time.setText(picker.getHour() + ":" + picker.getMinute());
     }
 }
