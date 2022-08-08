b90ff7337d74bb5c7de479fc30391730f6938f71
0ffz <dan.voznyy@gmail.com>
2022-08-08 08:48:39 -0400

    Delete unused class

--- app/src/main/java/com/example/cscb07/data/SignupResult.java
+++
@@ -1,9 +0,0 @@
-package com.example.cscb07.data;
-
-public class SignupResult {
-    public final boolean success;
-
-    public SignupResult(boolean success) {
-        this.success = success;
-    }
-}
