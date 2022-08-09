172710a75224836fb4f5c0bb14fef7526878099b
Gigi <gigi_cheang@hotmail.com>
2022-08-08 20:51:37 -0400

    Added max capacity field to DialogAddEvent.xml Added to strings.xml Added some functionality to AddEventViewModel.java

--- app/src/main/java/com/example/cscb07/ui/stateholders/AddEventViewModel.java
+++ app/src/main/java/com/example/cscb07/ui/stateholders/AddEventViewModel.java
@@ -6,10 +6,13 @@ import androidx.lifecycle.ViewModel;
 import com.example.cscb07.data.repositories.EventRepository;
 import com.example.cscb07.data.results.EventId;
 import com.example.cscb07.data.results.VenueId;
+import com.example.cscb07.data.util.MessageUtil;
 import com.example.cscb07.data.util.ServiceLocator;
 import com.example.cscb07.ui.state.TimeUiState;
 
 import java.util.Date;
+
+import io.vavr.control.Try;
 
 public class AddEventViewModel extends ViewModel {
     private final EventRepository eventRepository = ServiceLocator.getInstance().getEventRepository();
@@ -24,5 +27,25 @@ public class AddEventViewModel extends ViewModel {
     //TODO have separate class for handling messages
     private final MutableLiveData<Boolean> attemptingAddEvent = new MutableLiveData<>(false);
 
+    boolean validate(VenueId venue, String name, String description,
+                     String startDate, String startTime,
+                     String endDate, String endTime) {
+        //TODO
+        return false;
+    }
+
+    private void handleAddEventResult(Try<EventId> result) {
+        attemptingAddEvent.setValue(false);
+        result.onSuccess(createdEvent::postValue);
+        result.onFailure(MessageUtil::showError);
+    }
+
+    public void addEvent(VenueId venue, String name, String description,
+                         String startDate, String startTime,
+                         String endDate, String endTime) {
+        if(!validate(venue, name, description, startDate, startTime, endDate, endTime)) return;
+        if(attemptingAddEvent.getValue()) return;
+        eventRepository.addEvent(null, null, null, 0, 0, 0, this::handleAddEventResult);
+    }
     // notes: need setter and getters for startDate, startTime, endDate, endTime
 }
