129a4de0fb69e4caab798b70ad63d8667ed5df44
0ffz <dan.voznyy@gmail.com>
2022-08-09 20:09:06 -0400

    Fix up card colors

--- app/src/main/java/com/example/cscb07/ui/elements/screens/venuelist/VenueCard.java
+++ app/src/main/java/com/example/cscb07/ui/elements/screens/venuelist/VenueCard.java
@@ -2,16 +2,20 @@ package com.example.cscb07.ui.elements.screens.venuelist;
 
 import android.view.View;
 import android.widget.TextView;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 import com.example.cscb07.R;
+import com.google.android.material.card.MaterialCardView;
 
 public class VenueCard extends RecyclerView.ViewHolder {
     public TextView name;
     public TextView description;
+    public MaterialCardView cardView;
 
     public VenueCard(View view) {
         super(view);
         name = view.findViewById(R.id.venueTitle);
         description = view.findViewById(R.id.venueDescription);
+        cardView = view.findViewById(R.id.venueCardView);
     }
 }
